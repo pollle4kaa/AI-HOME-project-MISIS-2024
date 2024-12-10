5220ce8a0434a001e0e0e505061247eed2559401
dfetlyp <158744248+dfetlyp@users.noreply.github.com>
2024-12-10 22:54:03 +0300

    Delete model_pipeline.py

--- model_pipeline.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def save_model(model, filename='finalized_model.sav'):
-    with open(filename, 'wb') as f:
-        pickle.dump(model, f)
-
-def load_model(filename='finalized_model.sav'):
-    with open(filename, 'rb') as f:
-        return pickle.load(f)
